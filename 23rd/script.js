bbcff1b5e1a45da64766866a0a7e8a198bbdcd1f
Mohammad Kaif <iamkaif.india@gmail.com>
2024-12-15 20:43:05 +0530

    Add files via upload

---
+++ 23rd/script.js
@@ -0,0 +1,38 @@
+// let age = 19;
+// let grace = 2;
+
+// if ((age-grace) > 18) {
+//     console.log("You can drive");
+
+// }
+
+// else {
+//     console.log(
+//         "You are not able for drive cars"
+//     );
+
+// }
+
+// let age = 23;
+
+// if (age > 18) {
+//     console.log("you can have a passport");
+// }
+
+// else {
+//     console.log("you can not carry any passport in India")
+// }
+
+
+
+let mohammad = "good";
+
+if (mohammad == "good") {
+    console.log("you are allowed here");
+    
+}
+
+else {
+    console.log("get out from here");
+    
+}
